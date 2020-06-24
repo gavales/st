011505076066d7e08d9f2cacb68e79e9609fe3b5
StillANixRookie <gvales2831997@gmail.com>
2020-06-24 23:20:32 +0000

    Wed 24 Jun 2020 23:20:32 GMT

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 //static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-static char *font = "Iosevka:size=15:antialias=true:autohint=true";
+static char *font = "Iosevka:size=12:antialias=true:autohint=true";
 
 /* Spare fonts */
 static char *font2[] = {
