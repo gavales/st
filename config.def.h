706dca2fa65ae9d569d359d9aa042aa64fdf9bc0
StillANixRookie <gvales2831997@gmail.com>
2020-06-28 23:31:04 +0000

    Sun 28 Jun 2020 23:31:04 GMT

--- config.def.h
+++ config.def.h
@@ -10,7 +10,7 @@ static char *font2[] = {
 //	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true",
 };
 
-static int borderpx = 2;
+static int borderpx = 0;
 
 // What program is execed by st depends of these precedence rules:
 // 1: program passed with -e
--- config.h
+++ config.h
@@ -4,13 +4,13 @@
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 //static char *font = "Iosevka:size=17:antialias=true";
 
-/* Spare fonts */
+// Spare fonts
 static char *font2[] = {
-/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
-/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+//	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true",
+//	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true",
 };
 
-static int borderpx = 2;
+static int borderpx = 0;
 
 // What program is execed by st depends of these precedence rules:
 // 1: program passed with -e
